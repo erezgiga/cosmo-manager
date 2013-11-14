793de09005472f2f08fd691e670d63228563f80c
Ran Ziv <ran@gigaspaces.com>
2013-11-14 12:57:13 +0200

    fixed version bug

--- vagrant/versions.py
+++ vagrant/versions.py
@@ -3,7 +3,7 @@ __author__ = 'elip'
 COSMO_VERSION = "develop"
 
 FABRIC_RUNNER_VERSION = "develop"
-WORKER_INSTALLER_VERSION = "cosmo-widget-cosmofied"
+WORKER_INSTALLER_VERSION = "feature/cosmo-widget-cosmofied"
 RIEMANN_LOADER_VERSION = "develop"
 VAGRANT_PROVISION_VERSION = "develop"
 OPENSTACK_PROVISION_VERSION = "develop"
